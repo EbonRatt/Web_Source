f14f5656093f7b607a91caf0b49a412a34ba9135
Ebon <101968379+EbonRatt@users.noreply.github.com>
2024-04-27 17:53:45 +0700

    Update index.js

--- API/index.js
+++ API/index.js
@@ -27,6 +27,10 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage });
 
+app.get("/", function (req, res) {
+  res.json("Working");
+});
+
 app.post("/api/upload", upload.single("file"), function (req, res) {
   const file = req.file;
   res.status(200).json(file.filename);
